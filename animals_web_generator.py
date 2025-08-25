4b5e07436d03b9dd21e67315dc23893eabd42146
MisaelTox <misaeltoxcatl@gmail.com>
2025-08-25 14:04:32 +0200

    Finish project

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,11 +5,8 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-
-animals_data = load_data("animals_data.json")
-output = ""
-
-for animal in animals_data:
+def serialize_animal(animal):
+    output = ''
     output += '<li class="cards__item">\n'
     output += f'  <div class="card__title">{animal["name"]}</div>\n'
     output += '  <p class="card__text">\n'
@@ -23,7 +20,10 @@ for animal in animals_data:
 
     output += '  </p>\n'
     output += '</li>\n'
+    return output
 
+animals_data = load_data("animals_data.json")
+output = "".join(serialize_animal(animal) for animal in animals_data)
 
 with open("animals_template.html", "r") as html:
     template = html.read()
