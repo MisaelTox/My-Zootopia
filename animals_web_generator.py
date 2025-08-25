5a398923025d9d45563ebf3f3416ce1086d48ef6
MisaelTox <misaeltoxcatl@gmail.com>
2025-08-25 13:35:57 +0200

    HTML fixed for all animals

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,19 +7,22 @@ def load_data(file_path):
 
 
 animals_data = load_data("animals_data.json")
-
 output = ""
 
 for animal in animals_data:
-    if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+    output += '<li class="cards__item">\n'
+    output += f'  <div class="card__title">{animal["name"]}</div>\n'
+    output += '  <p class="card__text">\n'
+
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f'    <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f"Location: {animal ['locations'][0]}\n"
+        output += f'    <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
-    output += "\n"
+        output += f'    <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+
+    output += '  </p>\n'
+    output += '</li>\n'
 
 
 with open("animals_template.html", "r") as html:
