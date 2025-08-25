6037f04b2ce2656c4643545984873233004b0cda
MisaelTox <misaeltoxcatl@gmail.com>
2025-08-25 13:20:10 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,15 +8,24 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
+output = ""
+
 for animal in animals_data:
     if "name" in animal:
-        print(f"Name: {animal ['name']}")
+        output += f"Name: {animal['name']}\n"
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f"Diet: {animal['characteristics']['diet']}\n"
     if "locations" in animal and len(animal["locations"]) > 0:
-        print(f"Location: {animal ['locations'][0]}")
-    if "characteristics" in animal:
-        if "diet" in animal["characteristics"]:
-            print(f"Diet: {animal['characteristics']['diet']}")
-        if "type" in animal["characteristics"]:
-            print(f"Type: {animal['characteristics']['type']}")
+        output += f"Location: {animal ['locations'][0]}\n"
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f"Type: {animal['characteristics']['type']}\n"
+    output += "\n"
 
-    print()
+
+with open("animals_template.html", "r") as html:
+    template = html.read()
+
+new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as renew:
+    renew.write(new_html)
